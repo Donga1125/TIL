136463ec95b72583df1b371bcbef27900cadd5ea
Donga <ehddk1125@gmail.com>
2024-02-26 19:23:35 +0900

    ALGO: x만큼 간격이 있는 n개의 숫자

---
+++ Algorithm/S2024/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,18 @@
+package Algorithm.S2024;
+import java.util.*;
+
+public class x만큼간격이있는n개의숫자 {
+    public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+
+            long current = x;
+            for (int i = 0; i < n; i++) {
+                answer[i] = current;
+                current += x;
+            }
+
+            return answer;
+        }
+    }
+
+
