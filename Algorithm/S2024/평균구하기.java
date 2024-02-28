1af321bcfa97cac41e7747e28ade9100f920d146
Donga <ehddk1125@gmail.com>
2024-02-28 23:46:50 +0900

    ALGO: 평균구하기

---
+++ Algorithm/S2024/평균구하기.java
@@ -0,0 +1,17 @@
+package Algorithm.S2024;
+
+public class 평균구하기 {
+        public static double solution(int[] arr) {
+            int sum = 0;
+            for (int num : arr) {
+                sum += num;
+            }
+            return (double) sum / arr.length;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(solution(new int[]{1, 2, 3, 4})); // 2.5
+            System.out.println(solution(new int[]{5, 5}));       // 5.0
+        }
+
+}
