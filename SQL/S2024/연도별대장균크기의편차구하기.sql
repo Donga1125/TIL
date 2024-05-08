d4c099208f0053796cff3937f11b76b6a1cb81b1
Donga <ehddk1125@gmail.com>
2024-05-08 23:02:23 +0900

    SQL: 나이정보가 없는 회원 수 구하기, 연도별 대장균 크기의 편차 구하기 문제 수정

--- SQL/S2024/연도별대장균크기의편차구하기.sql
+++ SQL/S2024/연도별대장균크기의편차구하기.sql
@@ -2,16 +2,26 @@
 -- 분화된 연도별 대장균 크기의 편차는 분화된 연도별 가장 큰 대장균의 크기 - 각 대장균의 크기로 구하며 결과는 연도에 대해 오름차순으로 정렬하고 같은 연도에 대해서는 대장균 크기의 편차에 대해 오름차순으로 정렬해주세요.
 -- date_format으로 변환하면 string형이되서 select결과는 맞는데 답은 틀리게 나옵니다. year쓰셔야합니다. --> 라고 함
 
-WITH MAXS AS (
-    SELECT MAX(SIZE_OF_COLONY) AS MAXSIZE,
-           YEAR(DIFFERENTIATION_DATE) AS YEAR
-    FROM ECOLI_DATA
-    GROUP BY YEAR
+WITH MaxColonySize AS (
+    SELECT EXTRACT(YEAR FROM DIFFERENTIATION_DATE) AS YEAR,
+           MAX(SIZE_OF_COLONY) AS MAX_COLONY_SIZE
+      FROM ECOLI_DATA
+     GROUP BY EXTRACT(YEAR FROM DIFFERENTIATION_DATE)
+),
+YearlyDeviation AS (
+    SELECT E.ID,
+           EXTRACT(YEAR FROM E.DIFFERENTIATION_DATE) AS YEAR,
+           MC.MAX_COLONY_SIZE - E.SIZE_OF_COLONY AS YEAR_DEV
+      FROM ECOLI_DATA E
+           JOIN MaxColonySize MC
+             ON EXTRACT(YEAR FROM E.DIFFERENTIATION_DATE) = MC.YEAR
+),
+FinalResult AS (
+    SELECT YD.YEAR,
+           YD.YEAR_DEV,
+           YD.ID
+      FROM YearlyDeviation YD
+     ORDER BY YD.YEAR, YD.YEAR_DEV
 )
-SELECT M.YEAR,
-       (M.MAXSIZE - E.SIZE_OF_COLONY) AS YEAR_DEV,
-       E.ID
-FROM MAXS M
-INNER JOIN ECOLI_DATA E ON YEAR(E.DIFFERENTIATION_DATE) = M.YEAR
-WHERE YEAR(E.DIFFERENTIATION_DATE) = M.YEAR
-ORDER BY M.YEAR, YEAR_DEV;
+SELECT *
+  FROM FinalResult;
