283864294244c83000d0a87d6fb744b69b2e759a
Donga <ehddk1125@gmail.com>
2024-06-09 23:54:20 +0900

    ALGO: 가장 가까운 같은 글자

--- Algorithm/S2024/가장가까운같은글자.java
+++ Algorithm/S2024/가장가까운같은글자.java
@@ -1,4 +1,24 @@
 package Algorithm.S2024;
 
+import java.util.HashMap;
 public class 가장가까운같은글자 {
+    public static void main (String[]args){
+        System.out.println(solution("banana"	));
+}
+    public static int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        HashMap<Character, Integer> lastSeen = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (lastSeen.containsKey(currentChar)) {
+                answer[i] = i - lastSeen.get(currentChar);
+            } else {
+                answer[i] = -1;
+            }
+            lastSeen.put(currentChar, i);
+        }
+
+        return answer;
+    }
 }
