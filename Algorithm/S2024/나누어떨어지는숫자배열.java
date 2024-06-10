85812cc7ea67962024e676702366d0fae65b6c3a
Donga <ehddk1125@gmail.com>
2024-06-10 23:07:06 +0900

    ALGO: 나누어떨어지는숫자배열

---
+++ Algorithm/S2024/나누어떨어지는숫자배열.java
@@ -0,0 +1,30 @@
+package Algorithm.S2024;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 나누어떨어지는숫자배열 {
+    public static void main (String[]args){
+        System.out.println(solution(new int[]{5, 9, 7, 10}, 5));
+    }
+
+    public static int[] solution(int[] arr, int divisor) {
+        List<Integer> divisibleNumbers = new ArrayList<>();
+
+        for (int num : arr) {
+            if (num % divisor == 0) {
+                divisibleNumbers.add(num);
+            }
+        }
+
+        if (divisibleNumbers.isEmpty()) {
+            return new int[]{-1};
+        } else {
+            // ArrayList를 배열로 변환하고 오름차순으로 정렬
+            int[] result = divisibleNumbers.stream().mapToInt(i -> i).toArray();
+            Arrays.sort(result);
+            return result;
+        }
+    }
+}
