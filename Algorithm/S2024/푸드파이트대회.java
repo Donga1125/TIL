af1fca1f6cab4f106f516e2e8d5df7171b98d79c
Donga1125 <ehddk0133@naver.com>
2024-06-11 22:26:02 +0900

    ALGO: 푸트파이트 대회

---
+++ Algorithm/S2024/푸드파이트대회.java
@@ -0,0 +1,13 @@
+package Algorithm.S2024;
+
+public class 푸드파이트대회 {
+    public static void main (String [] args){
+        System.out.println(solution(new int[]{1, 3, 4, 6}));
+        System.out.println(solution(new int[]{1, 7, 1, 2}));
+    }
+
+    public static String solution(int[] food) {
+        String answer = "";
+        return answer;
+    }
+}
