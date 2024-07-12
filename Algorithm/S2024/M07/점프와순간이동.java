e94b007f4510a196f78ecbc6a17d74f483a7b0d6
Donga1125 <ehddk0133@naver.com>
2024-07-12 23:21:12 +0900

    ALGO: 점프와 순간이동

---
+++ Algorithm/S2024/M07/점프와순간이동.java
@@ -0,0 +1,25 @@
+package Algorithm.S2024.M07;
+
+public class 점프와순간이동 {
+    public static void main (String[] args){
+
+        System.out.println(solution(5));
+        System.out.println(solution(6));
+        System.out.println(solution(5000));
+    }
+    public static int solution(int n) {
+        int batteryUsage = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                batteryUsage++;
+            }
+        }
+
+        return batteryUsage;
+    }
+
+}
