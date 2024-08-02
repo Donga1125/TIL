2c1b6c5074e9f219e6f30d50373b16cc838cadc3
Donga1125 <ehddk0133@naver.com>
2024-08-03 00:02:22 +0900

    ALGO: 모음사전

--- Algorithm/S2024/M08/모음사전.java
+++ Algorithm/S2024/M08/모음사전.java
@@ -1,4 +1,23 @@
 package Algorithm.S2024.M08;
 
 public class 모음사전 {
+    public static void main (String[] args){
+        System.out.println(solution("AAAAE"));
+        System.out.println(solution("AAAE"));
+    }
+
+    public static int solution(String word) {
+
+        String vowels = "AEIOU";
+        int[] multiplier = {781, 156, 31, 6, 1};
+
+        int index = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            int position = vowels.indexOf(word.charAt(i));
+            index += position * multiplier[i] + 1;
+        }
+
+        return index;
+    }
 }
