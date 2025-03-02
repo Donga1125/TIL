a5344609046ef87055f7accbbc830e4d46980e3d
Donga <ehddk1125@gmail.com>
2025-03-02 23:51:10 +0900

    ALGO: 바탕화면 정리

--- Algorithm/S2025/M03/바탕화면정리.java
+++ Algorithm/S2025/M03/바탕화면정리.java
@@ -1,4 +1,35 @@
 package Algorithm.S2025.M03;
 
+import java.util.Arrays;
+
 public class 바탕화면정리 {
+    public static void main (String [] args){
+
+        String[] str1 = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
+        String[] str2 = {".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."};
+        String[] str3 = {".#...", "..#..", "...#."};
+        System.out.println(Arrays.toString(solution(str1)));
+        System.out.println(Arrays.toString(solution(str2)));
+        System.out.println(Arrays.toString(solution(str3)));
+
+    }
+
+    public static int[] solution(String[] wallpaper) {
+        int lux = Integer.MAX_VALUE, luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE, rdy = Integer.MIN_VALUE;
+
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
+                    }
+            }
+        }
+
+        return new int[]{lux, luy, rdx + 1, rdy + 1};
+    }
 }
+
